574cb10d8e11dcef8d0908f1cf30040a2ea0e7a9
Nishant <Nishantawesom@hotmail.com>
2019-12-08 00:30:38 +0700

    Till Auth Section

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -8,18 +8,18 @@
                    parent::initialize();
                    $this->loadComponent('Paginator');
                     $this->loadComponent('Flash'); // Include the FlashComponent
+                    $this->Auth->allow(['tags']);
                 }
                public function index()
                 {
                       $articles = $this->Paginator->paginate($this->Articles->find());
                        $this->set(compact('articles'));
                 }
-                  public function view($slug)
-                  {
-                    $article = $this->Articles->findBySlug($slug)->contain(['Tags'])
-                    ->firstOrFail();
-                    $this->set(compact('article'));
-                  }
+                public function view($slug = null)
+                {
+                $article = $this->Articles->findBySlug($slug)->firstOrFail();
+                $this->set(compact('article'));
+                }
 
                   public function add()
                   {
@@ -28,7 +28,10 @@
                   $article = $this->Articles->patchEntity($article, $this->request->getData());
                   // Hardcoding the user_id is temporary, and will be removed later
                   // when we build authentication out.
-                  $article->user_id = 1;
+                  
+                  // Changed: Set the user_id from the session.
+                  $article->user_id = $this->Auth->user('id');
+                  
                   if ($this->Articles->save($article)) {
                   $this->Flash->success(__('Your article has been saved.'));
                   return $this->redirect(['action' => 'index']);
@@ -47,22 +50,22 @@
                   
 public function edit($slug)
 {
-    $article = $this->Articles->findBySlug($slug)->contain(['Tags'])
-    ->firstOrFail();
+$article = $this->Articles
+->findBySlug($slug)
+->contain('Tags') // load associated Tags
+->firstOrFail();
 if ($this->request->is(['post', 'put'])) {
-$this->Articles->patchEntity($article, $this->request->getData());
+$this->Articles->patchEntity($article, $this->request->getData(), [
+// Added: Disable modification of user_id.
+'accessibleFields' => ['user_id' => false]
+]);
 if ($this->Articles->save($article)) {
 $this->Flash->success(__('Your article has been updated.'));
 return $this->redirect(['action' => 'index']);
 }
 $this->Flash->error(__('Unable to update your article.'));
 }
-// Get a list of tags.
-$tags = $this->Articles->Tags->find('list');
-// Set tags to the view context
-$this->set('tags', $tags);
 $this->set('article', $article);
-
 }
                
                
@@ -93,6 +96,24 @@ $this->set([
 'articles' => $articles,
 'tags' => $tags
 ]);
+}
+
+
+public function isAuthorized($user)
+{
+$action = $this->request->getParam('action');
+// The add and tags actions are always allowed to logged in users.
+if (in_array($action, ['add', 'tags'])) {
+return true;
+}
+// All other actions require a slug.
+$slug = $this->request->getParam('pass.0');
+if (!$slug) {
+return false;
+}
+// Check that the article belongs to the current user.
+$article = $this->Articles->findBySlug($slug)->first();
+return $article->user_id === $user['id'];
 }
 
    }   
